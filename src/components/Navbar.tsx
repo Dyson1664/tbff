07722cec7b9a1d5631c0039963021a1be820cc8b
Dyson1664 <127164393+Dyson1664@users.noreply.github.com>
2025-08-31 11:38:24 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -127,7 +127,7 @@ const Navbar = memo(() => {
           {/* Logo/Home Button */}
           <Link to="/">
             <Button variant="link" className="text-xl font-bold text-primary hover:no-underline p-0 flex items-center gap-3">
-              <img src={ibtLogo} alt="IBT Logo" className="h-17 w-17" />
+              <img src={ibtLogo} alt="IBT Logo" className="h-16 w-16" />
               Imagine Beyond Travel
             </Button>
           </Link>
